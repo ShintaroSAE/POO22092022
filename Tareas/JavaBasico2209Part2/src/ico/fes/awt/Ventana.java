65590f57d952e9f24e643a1b46f4fbf8a75e4378
ShintaroSAE <eduardosalazar93@aragon.unam.mx>
2022-05-06 21:50:51 -0500

    clase 05/05/22

--- Tareas/JavaBasico2209Part2/src/ico/fes/awt/Ventana.java
+++ Tareas/JavaBasico2209Part2/src/ico/fes/awt/Ventana.java
@@ -1,20 +1,42 @@
 package ico.fes.awt;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.HeadlessException;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * @author Eduardo Salazar
  */
-public class Ventana extends Frame{
-    String titulo;
-
+public class Ventana extends Frame implements MouseListener{
+    private String titulo;
+    private Button boton1;
+    private FlowLayout layout;
+    private Label etiqueta;
+    private TextField cuadroTexto;
+    
     public Ventana() throws HeadlessException {
     }
 
     public Ventana(String titulo) throws HeadlessException {
         super(titulo);
         this.setSize(600,400);
+        layout = new FlowLayout(FlowLayout.LEFT);
+        this.setLayout(layout);
+        boton1 = new Button("Presioname");
+        cuadroTexto = new TextField(15);
+        etiqueta = new Label("Texto inicial");
+        this.add(cuadroTexto);
+        this.add(boton1);
+        this.add(etiqueta);
+        
+        this.boton1.addMouseListener(this);
+        
+        
         this.setVisible(true);
     }
 
@@ -29,6 +51,31 @@ public class Ventana extends Frame{
     @Override
     public String toString() {
         return "Ventana{" + "titulo=" + titulo + '}';
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        etiqueta.setText("Hola " + cuadroTexto.getText());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
     }
     
 }
--- Tareas/JavaBasico2209Part2/src/javabasico2209part2/JavaGraficoMain.java
+++ Tareas/JavaBasico2209Part2/src/javabasico2209part2/JavaGraficoMain.java
@@ -1,13 +1,14 @@
 package javabasico2209part2;
 
 import ico.fes.awt.Ventana;
+import ico.fes.awt.VentanaVersion2;
 
 /**
  * @author Eduardo Salazar
  */
 public class JavaGraficoMain {
     public static void main(String[] args) {
-        Ventana ventana = new Ventana("Hola AWT ICO FES Aragon");
-        
+        //Ventana ventana = new Ventana("Hola AWT ICO FES Aragon");
+        VentanaVersion2 ventana = new VentanaVersion2();
     }
 }
