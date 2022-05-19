7cb1948b1ebd3371b1ef944854849d2f9bfcb6ee
ShintaroSAE <eduardosalazar93@aragon.unam.mx>
2022-05-19 12:58:11 -0500

    clase 19/05/22

---
+++ Tareas/JavaBasico2209Part2/src/ico/fes/swing/ItemAdapter.java
@@ -0,0 +1,16 @@
+package ico.fes.swing;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+/**
+ *
+ * @author Eduardo Salazar
+ */
+public class ItemAdapter implements ItemListener{
+
+    @Override
+    public void itemStateChanged(ItemEvent ie) {
+    }
+    
+}
--- Tareas/JavaBasico2209Part2/src/ico/fes/swing/VentanaSwing.java
+++ Tareas/JavaBasico2209Part2/src/ico/fes/swing/VentanaSwing.java
@@ -1,18 +1,24 @@
 package ico.fes.swing;
 
+import ico.fes.herencia.Persona;
+import ico.fes.modelo.ModeloPersonaCombo;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ItemEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -23,11 +29,15 @@ public class VentanaSwing extends JFrame{
     private JTextField cuadro;
     private JButton botón;
     private JLabel resultado;
+    private JComboBox<Persona> lista;
+    private ModeloPersonaCombo modelo;
+    private JTextArea texto;
     
     public VentanaSwing() throws HeadlessException {
         setTitle("Conversión de grados ºC a ºF");
         setSize(400, 300);
         setLayout(new FlowLayout(FlowLayout.CENTER));
+        
         cuadro = new JTextField(5);
         ImageIcon icono = new ImageIcon( System.getProperty("user.dir") + "\\src\\ico\\fes\\swing\\prosuna.png");
         botón = new JButton(icono);
@@ -35,15 +45,34 @@ public class VentanaSwing extends JFrame{
         botón.setOpaque(true);
         botón.setToolTipText("Clic para convertir en ºF");
         resultado = new JLabel("ºF");
+        lista = new JComboBox();
+        texto = new JTextArea(5,20);
+        /*lista.addItem("Ingeniería");
+        lista.addItem("Derecho");
+        lista.addItem("Periodismo");
+        lista.addItem("Arquitectura");*/
+        modelo = new ModeloPersonaCombo();
+        modelo.consultarBaseDatos();
+        lista.setModel(modelo);
         this.getContentPane().add(cuadro);
         this.getContentPane().add(botón);
         this.getContentPane().add(resultado);
+        this.getContentPane().add(lista);
+        this.getContentPane().add(texto);
         this.validate();
         this.setVisible(true);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e){
                 System.exit(0);
+            }
+        });
+        
+        this.lista.addItemListener(new ItemAdapter(){
+            @Override
+            public void itemStateChanged(ItemEvent ie) {
+                System.out.println("evento..."+ ie.getItem());
+                texto.setText(ie.getItem()+ "\n");
             }
         });
         
